f8d06570db182df4c67f3a82b5c5c6dd29cb7c62
sharonkorir <csharonkorir@gmail.com>
2023-09-21 17:01:44 +0300

    [delivers #186079844] Updated images and styled login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -8,111 +8,115 @@ import youtube from "../../assets/youtube.png";
 import telephone from "../../assets/telephone.png";
 import pin from "../../assets/pin.png";
 import mail from "../../assets/mail.png";
+import logo from "../../assets/marble.png";
+import google from "../../assets/google.png";
 
 export default function Login() {
   return (
     <div className="container">
       <nav className="nav">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-          }}
-        >
-          <p>Logo</p>
-          <p>Home</p>
-          <p>About</p>
-          <p>Login</p>
+        <div className="flex">
+          <div className=" center logo-box">
+            <div className="center">
+              <p className="link">PYO INFOTECH</p>
+            </div>
+            <img src={logo} />
+          </div>
+          <div className="center">
+            <p className="link">Home</p>
+            <p className="link">About</p>
+            <p className="link">Login</p>
+          </div>
         </div>
-        <div>
-          <p>Logout</p>
+        <div className="center">
+          <p className="link">LOGOUT</p>
         </div>
       </nav>
       <div className="hero">
         <div className="card">
-          <h2>pro tech</h2>
+          <div
+            style={{
+              color: "white",
+              textAlign: "center",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <div>
+              <p className="link">PYO INFOTECH</p>
+            </div>
+            <img src={logo} />
+          </div>
           <form>
-            <button>
-              <img />
-              Sign in with Google
+            <button className="google">
+              <img src={google} />
+              <div className="center">
+                <p>Sign in with Google</p>
+              </div>
             </button>
             <label>Email</label>
             <input placeholder="email" required />
             <label>Password</label>
             <input placeholder="password" required />
-            <button>SIGN IN</button>
-            <p>
-              Not a member yet? <span>Resgister now!</span>
+            <button className="login-button">SIGN IN</button>
+            <p className="center">
+              Not a member yet? <span className="blue"> Resgister now!</span>
             </p>
           </form>
         </div>
       </div>
-      <footer className="footer">
-        <div>
-          <div>
-            <h2>Get in Touch with us!</h2>
-            <div>
+      <footer>
+        <div
+          style={{
+            display: "flex",
+            padding: "20px",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ width: "50%", paddingLeft: "40px" }}>
+            <h2 style={{ paddingLeft: "20px" }}>Get in Touch with us!</h2>
+            <div className="footer-section">
               <img src={pin} />
               <p>
                 2nd Floor, Tridav Complex, above Rathi X ray & Sonography
                 Clinic, Udhana Darwaja Surat, Gujarat 395002
               </p>
             </div>
-            <div>
+            <div className="footer-section">
               <img src={mail} />
               <p>info@something.com</p>
             </div>
-            <div>
+            <div className="footer-section">
               <img src={telephone} />
               <div>
-                <p>=91777778976(India)</p>
-                <p>=91777778976(France)</p>
-                <p>=91777778976(Canada)</p>
+                <p>+91 777778976 (India)</p>
+                <p>+33 777778976 (France)</p>
+                <p>+1(703) 777778976 (Canada)</p>
               </div>
             </div>
           </div>
-          <div className="card">
+          <div className="subscribe">
             <form>
-              <h6>Subscribe to our Newsletter </h6>
+              <p>Subscribe to our Newsletter </p>
               <label>Enter your email address here!</label>
               <input placeholder="Email Address" />
-              <button>Subscribe</button>
+              <button className="logo-box">Subscribe</button>
             </form>
           </div>
         </div>
-        <div>
-          <a
-            href="https://www.flaticon.com/free-icons/facebook"
-            title="facebook icons"
-          >
-            <img src={facebook} alt="" />
-          </a>
-          <a
-            href="https://www.flaticon.com/free-icons/instagram"
-            title="instagram icons"
-          >
-            <img src={instagram} alt="" />
-          </a>
-          <a
-            href="https://www.flaticon.com/free-icons/twitter-social-badge"
-            title="twitter social badge icons"
-          >
-            <img src={twitter} alt="" />
-          </a>
-          <a
-            href="https://www.flaticon.com/free-icons/linkedin"
-            title="linkedin icons"
-          >
-            <img src={linkedin} alt="" />
-          </a>
-          <a
-            href="https://www.flaticon.com/free-icons/youtube"
-            title="youtube icons"
-          >
-            <img src={youtube} alt="" />
-          </a>
+
+        <div className="footer-icons">
+          <img src={facebook} alt="" />
+
+          <img src={instagram} alt="" />
+
+          <img src={twitter} alt="" />
+
+          <img src={linkedin} alt="" />
+
+          <img src={youtube} alt="" />
         </div>
-        <div>
+        <div className="footer-links">
           <p>Home</p>
           <p>About</p>
           <p>Contact</p>
