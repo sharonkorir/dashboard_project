951cc240f71ada72911dd755b4c22fa27c5ecba8
sharonkorir <csharonkorir@gmail.com>
2023-09-21 10:44:51 +0300

    create initial page layout

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import "./login.css";
+
+export default function Login() {
+  return (
+    <div className="container">
+      <nav className="nav">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+          }}
+        >
+          <p>Logo</p>
+          <p>Home</p>
+          <p>About</p>
+          <p>Login</p>
+        </div>
+        <div>
+          <p>Logout</p>
+        </div>
+      </nav>
+      <div className="hero">
+        <div className="card">
+          <h2>pro tech</h2>
+          <form>
+            <button>
+              <img />
+              Sign in with Google
+            </button>
+            <label>Email</label>
+            <input placeholder="email" required />
+            <label>Password</label>
+            <input placeholder="password" required />
+            <button>SIGN IN</button>
+            <p>
+              Not a member yet? <span>Resgister now!</span>
+            </p>
+          </form>
+        </div>
+      </div>
+      <footer className="footer">
+        <div>
+          <div>
+            <h2>Get in Touch with us!</h2>
+            <div>
+              <img />
+              <p>
+                2nd Floor, Tridav Complex, above Rathi X ray & Sonography
+                Clinic, Udhana Darwaja Surat, Gujarat 395002
+              </p>
+            </div>
+            <div>
+              <img />
+              <p>info@something.com</p>
+            </div>
+            <div>
+              <img />
+              <div>
+                <p>=91777778976(India)</p>
+                <p>=91777778976(France)</p>
+                <p>=91777778976(Canada)</p>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <form>
+              <h6>Subscribe to our Newsletter </h6>
+              <label>Enter your email address here!</label>
+              <input placeholder="Email Address" />
+              <button>Subscribe</button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>Home</p>
+          <p>About</p>
+          <p>Contact</p>
+          <p>Login</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
