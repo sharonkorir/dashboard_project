efb777fc9ea09e4f8ba67a508f82e893177f3592
sharonkorir <csharonkorir@gmail.com>
2023-10-06 16:26:24 +0300

    fix(sign up ui): update images, fonts, colors and spacing

--- src/components/DarkButton.jsx
+++ src/components/DarkButton.jsx
@@ -8,13 +8,12 @@ export const DarkButton = ({ text, icon, width }) => {
       width={width}
       bg={theme.colors.bg.black}
       color={theme.colors.bg.grey}
-      sx={{
-        "&:hover": {
-          backgroundColor: theme.colors.bg.hoverBlack,
-          color: theme.colors.bg.hoverGrey,
-        },
+      _hover={{
+        backgroundColor: theme.colors.bg.hoverBlack,
+        color: theme.colors.bg.hoverGrey,
       }}
       leftIcon={icon ? <Icon as={icon} boxSize={"1.5rem"} /> : undefined}
+      fontWeight="medium"
     >
       {text}
     </Button>
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,23 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   colors: {
     bg: {
-      black: "blackAlpha.800",
+      black: "rgb(72 72 72)",
       hoverBlack: "blackAlpha.900",
-      grey: "gray.200",
+      grey: "rgb(238 238 238)",
       hoverGrey: "gray.100",
-      blue: "blue.600",
+      blue: "rgb(77 126 220)",
+      green: "rgb(72 72 72)",
+      lightGrey: "rgb(238 238 238)",
+    },
+    text: {
+      grey: "rgb(86 91 94)",
+    },
+    button: {
+      50: "#29cc97",
+
+      500: "#29cc97",
+
+      900: "#29cc97",
     },
   },
 });
